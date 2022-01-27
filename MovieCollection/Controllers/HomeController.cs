014533a4f77cc21e825c012f4d885924c62f51d4
bkimb12 <braedonk@byu.edu>
2022-01-26 23:10:26 -0700

    Add project files.

---
+++ MovieCollection/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MovieCollection.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieCollection.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private MovieDatabaseContext movieContext { get; set; }
+
+        public HomeController(ILogger<HomeController> logger, MovieDatabaseContext someName)
+        {
+            _logger = logger;
+            movieContext = someName;
+        }
+
+        //home page
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //podcast page
+        public IActionResult Podcasts()
+        {
+            return View();
+        }
+
+        //get for add movie page
+        [HttpGet]
+        public IActionResult AddMovie()
+        {
+            return View();
+        }
+
+        //post for add movie page
+        [HttpPost]
+        public IActionResult AddMovie (AddMovie am)
+        {
+            movieContext.Add(am);
+            movieContext.SaveChanges();
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MovieCollection/Models/MovieDatabaseContext.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieCollection.Models
+{
+    public class MovieDatabaseContext : DbContext
+    {
+        //Constructor
+        public MovieDatabaseContext(DbContextOptions<MovieDatabaseContext> options) : base (options)
+        { 
+            
+        }
+
+        public DbSet<AddMovie> Responses { get; set; }
+
+        //seed database w/3 movies
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            mb.Entity<AddMovie>().HasData(
+
+                new AddMovie
+                { 
+                    MovieID = 1,
+                    Category = "Sports",
+                    Title = "Hoosiers",
+                    Year = 1986, 
+                    Director = "David Anspaugh",
+                    Rating = "PG",
+                    Edited = false,
+                    LentTo = "", 
+                    Notes = ""
+                },
+                new AddMovie
+                {
+                    MovieID = 2,
+                    Category = "Family",
+                    Title = "Encanto",
+                    Year = 2021,
+                    Director = "Byron Howard, Jared Bush",
+                    Rating = "PG",
+                    Edited = false,
+                    LentTo = "",
+                    Notes = ""
+                },
+                new AddMovie
+                {
+                    MovieID = 3,
+                    Category = "Action/Adventure",
+                    Title = "Midway",
+                    Year = 2019,
+                    Director = "Roland Emmerich",
+                    Rating = "PG-13",
+                    Edited = false,
+                    LentTo = "",
+                    Notes = ""
+                }
+            );
+        }
+    }
+}
